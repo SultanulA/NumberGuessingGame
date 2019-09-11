98f52771b5d2bd015623fdfd0437df149601f4c8
mvdoyle <mdoyle@truecoders.io>
2019-09-11 08:42:28 -0500

    created program

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -4,9 +4,58 @@ namespace NumberGuessingGame
 {
     class Program
     {
+        static int GetIntegerFromUser(string question)
+        {
+            int integerFromUser;
+            bool success;
+            do
+            {
+                Console.WriteLine(question);
+                string userResponse = Console.ReadLine();
+                success = int.TryParse(userResponse, out integerFromUser);
+            } while (success == false);
+
+            return integerFromUser;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's play a guessing game!  The higher your score, the worse you did!");
+
+            int max = GetIntegerFromUser("What max range would you like?");
+
+            Random rnd = new Random();
+
+            int secretNumber = rnd.Next(1, max + 1);
+
+            int score = 0;
+            int guess;
+
+            do
+            {
+                Console.ResetColor();
+                Console.WriteLine("Your current score is " + score);
+                guess = GetIntegerFromUser("Please guess a number between 1-" + max + ":");
+
+                if (guess > secretNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You were too high, loser!");
+                    score += 1;
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You were too low, loser");
+                    score += 1;
+                }
+
+            } while (guess != secretNumber);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You got it!");
+            Console.WriteLine("Your final score was " + score);
         }
     }
 }
